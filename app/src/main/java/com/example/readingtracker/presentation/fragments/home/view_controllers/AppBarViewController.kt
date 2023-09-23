0e5e040476f5903e56c6f4fa648248ab611d7f79
nasty <nastyakras1707@gmail.com>
2023-09-23 10:49:49 +0300

    Set elevation for Home Screen

--- app/src/main/java/com/example/readingtracker/presentation/fragments/home/view_controllers/AppBarViewController.kt
+++ app/src/main/java/com/example/readingtracker/presentation/fragments/home/view_controllers/AppBarViewController.kt
@@ -8,7 +8,7 @@ class AppBarViewController(
     fun setUpViews() {
         binding.homeNestedScrollView.setOnScrollChangeListener { v, _, _, _, _ ->
             binding.homeActionBar.topNavigationBar.elevation =
-                if (v.canScrollVertically(SCROLL_DIRECTION_UP)) 10f else 0f
+                if (v.canScrollVertically(SCROLL_DIRECTION_UP)) 4f else 0f
         }
     }
 
